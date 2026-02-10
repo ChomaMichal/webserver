0e9ef7530d93467808355fda10d199b4515f1f75
Michal Choma <michalkk04@gmail.com>
2026-02-10 18:17:20 +0100

    added to clinet now it can call request

--- client/Client.hpp
+++ client/Client.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "../sockets/Sockets.hpp"
+#include "../utils/result/Result.hpp"
 #include "Request.hpp"
 
 class Client {
@@ -8,6 +9,12 @@ public:
   ~Client();
   Client(const Client &obj);
   Client &operator=(const Client &obj);
+
+  // returns false if there is no request true is request was susccesfuly
+  // recieved and parsed error is set in result
+  Result<bool> recieveRequest(void);
+  void close(void);
+  Request &getRequest(void);
 
 private:
   Stream _stream;
