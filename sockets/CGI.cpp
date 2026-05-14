fbed3b100ff3502e34806c2180846f4187da78a5
Michal Choma <michalkk04@gmail.com>
2026-05-14 17:10:02 +0200

    fix: fixed cgi pipes

--- sockets/CGI.cpp
+++ sockets/CGI.cpp
@@ -1,4 +1,5 @@
 #include "CGI.hpp"
+#include <stdexcept>
 #include <unistd.h>
 
 CGI::CGI(int fd) : fd(fd) {
@@ -15,15 +16,25 @@ Result<CGI> CGI::run_script(const char *script_path,
                             const char *interpteret_path) {
   char *args[3];
 
-  args[0] = (char *)interpteret_path;
-  args[1] = (char *)script_path;
-  args[2] = NULL;
-  int let = execve(interpteret_path, args, NULL);
-  if (let == -1) {
-    return Result<CGI>(std::string("execve failed"));
+  int fds[2];
+
+  if (pipe(fds) == -1) {
+    return Result<CGI>(std::string("pipe failed"));
+  };
+  int pid = fork();
+  if (pid == -1)
+    return Result<CGI>(std::string("fork failed"));
+  if (pid == 0) {
+    args[0] = (char *)interpteret_path;
+    args[1] = (char *)script_path;
+    args[2] = NULL;
+    dup2(fds[1], STDOUT_FILENO);
+    int let = execve(interpteret_path, args, NULL);
+    throw std::runtime_error("execve failed");
+  } else {
+    CGI ret(fds[0]);
+    return Result<CGI>(ret);
   }
-  CGI ret(let);
-  return Result<CGI>(ret);
 }
 
 CGI &CGI::operator=(const CGI &other) {
